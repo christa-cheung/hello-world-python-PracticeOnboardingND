99234d86807c6e74fbcbc02ab430ecbfd0031c4c
christa-cheung <54732090+christa-cheung@users.noreply.github.com>
2019-09-03 13:10:31 -0700

    Add files via upload

---
+++ hello_world_student.py
@@ -0,0 +1,25 @@
+"""
+Python 3.7.4
+hello_world_REPLACE.py
+
+DATE
+
+DESCRIPTION OF THE PROGRAM
+"""
+# Write a print function that prints out the string "Hello World".
+
+
+# Add to the print function your name after "Hello World".
+
+
+#Stretch Exercise
+# What happens when the print function takes in the integer 5?
+
+
+# Write a print function containing a sentence.
+
+
+# Try out the print function in a way that hasn't been stated.
+#   i.e. What happens when the print function takes in a boolean, True?
+
+
